f6604e6b6546d84862dec8bc46d3537d71f4aea1
z123fg <zyz1029@gmail.com>
2023-07-08 00:04:41 -0500

    Update peerConnection.ts

--- src/utils/peerConnection.ts
+++ src/utils/peerConnection.ts
@@ -1,90 +1,111 @@
-const servers = {
-  iceServers: [
-    {
-      urls: ['stun:stun1.l.google.com:19302', 'stun:stun2.l.google.com:19302'],
-    },
-  ],
-  iceCandidatePoolSize: 10,
-};
+class PeerConnection {
+    pc: RTCPeerConnection | undefined;
+    isMaster: boolean;
+    state: string;
+    timeout: number;
+    dc: RTCDataChannel | null = null;
+    onUpdateIce: Function;
+    static server: Object;
+    constructor(
+        isMaster: boolean,
+        { timeout, onUpdateIce } = { timeout: 3000, onUpdateIce: () => {} }
+    ) {
+        this.isMaster = isMaster;
+        this.state = "pending";
+        this.timeout = timeout;
+        this.onUpdateIce = onUpdateIce;
+        this.init();
+    }
 
+    init() {
+        this.pc = new RTCPeerConnection();
+        if (this.isMaster) this.dc = this.pc.createDataChannel("dc");
+        this.state = "pending";
+    }
 
-export const createPeerConnection = () => new RTCPeerConnection(servers);
+    initListener() {
+        this.pc!.onconnectionstatechange = () => {
+            const state = this.pc!.connectionState;
+            this.state = state;
+            if (state === "disconnected" || state === "failed") {
+            }
+        };
+    }
 
+    async initOffer(): Promise<void> {
+        if (this.state === "middle")
+            throw Error("peer connection cannot be initialized twice!");
+        if (this.isMaster === false)
+            throw Error("cannot init offer from a slave!");
+        try {
+            const timeout = setTimeout(() => {
+                console.log("create offer timeout, " + timeout + "ms");
+                this.state = "pending";
+                this.initOffer();
 
+            }, this.timeout);
+            const offer = await this.pc!.createOffer();
+            this.pc!.setLocalDescription(offer);
+            this.state
+            this.pc!.onicecandidate = (e: RTCPeerConnectionIceEvent) => {
+                if (e.candidate === null) {
+                    console.log("offer created", this.pc!.localDescription);
+                    clearTimeout(timeout);
+                    this.state = "middle";
+                    const sessionDescription = this.pc!.localDescription;
 
+                    if (sessionDescription === null)
+                        throw Error("failed to get generate answer!");
+                    this.onUpdateIce(JSON.stringify(sessionDescription));
+                }
+            };
+        } catch (err) {
+            this.state = "pending";
+            this.initOffer();
+        }
+    }
 
+    connect(answerString: string) {
+        if (this.isMaster === false)
+            throw Error("cannot complete connection from slave side");
 
+        const answer: RTCSessionDescription = JSON.parse(answerString);
+        this.pc!.setRemoteDescription(answer);
+    }
 
-
-// export const createPeerConnection = async (onMessageCb: Function) => {
-//   const pc = new RTCPeerConnection(servers);
-//   const dc = pc.createDataChannel('channel');
-
-//   dc.onmessage = (e) => {
-//     console.log('Received a message: ', e.data);
-//     onMessageCb(e.data);
-//   };
-
-//   dc.onopen = (e) => {
-//     console.log('Connection opened!');
-//   };
-
-//   // will be called when localdescription is set
-//   pc.onicecandidate = (e) => {
-//     console.log('New ICE candidate! Printing SDP...\n' + JSON.stringify(pc.localDescription));
-//   };
-//   const offer = await pc.createOffer();
-//   pc.setLocalDescription(offer);
-//   // TODO: send local description to server and get a room id
-//   let roomId;
-//   return [pc, dc, roomId];
-// };
-
-export const connectToRemote = (roomId: string) => {
-  // TODO: get remote description from the server
-  // pc.setRemoteDescription(JSON.parse(otherIDRef.current.value)).then(
-  //   console.log("Answer received")
-  // );
-};
-
-export const joinPeerConnection = (
-  roomId: string,
-  onMessageCb: Function,
-): [RTCPeerConnection, RTCDataChannel | null] => {
-  const pc = new RTCPeerConnection(servers);
-  let dc: RTCDataChannel | null = null;
-
-  // will be called when localdescription is set
-  pc.onicecandidate = (e) => {
-    console.log('New ICE candidate! Printing SDP...\n' + JSON.stringify(pc.localDescription));
-  };
-
-  pc.ondatachannel = (e) => {
-    dc = e.channel;
-    dc.onmessage = (e) => {
-      console.log('Received a message: ', e.data);
-      onMessageCb(e.data);
-    };
-    dc.onopen = () => {
-      console.log('Connection opened!!!');
-    };
-  };
-
-  // TODO: request remote description from server
-  // pc.setRemoteDescription(JSON.parse(otherIDRef.current.value)).then(
-  //   console.log("offer set!")
-  // );
-
-  pc.createAnswer()
-    .then((answer) => {
-      pc.setLocalDescription(answer);
-    })
-    .then(() => {
-      console.log('Answer created. Set local description successfully!');
-    });
-  return [pc, dc];
-};
-
-export const send = (data: string, dc: RTCDataChannel) => {
-  dc.send(data);
-};
+    async initAnswer(offer: string): Promise<string> {
+        if (this.state === "middle")
+            throw Error("peer connection cannot be initialized twice!");
+        if (this.isMaster === true)
+            throw Error("cannot init answer from a master!");
+        try {
+            const answer = await new Promise<RTCSessionDescription>(
+                async (res, rej) => {
+                    const timeout = setTimeout(() => {
+                        rej("create answer timeout, " + timeout + "ms");
+                    }, this.timeout);
+                    this.pc!.setRemoteDescription(JSON.parse(offer));
+                    const answer = await this.pc!.createAnswer();
+                    this.pc!.setLocalDescription(answer);
+                    this.pc!.onicecandidate = (
+                        e: RTCPeerConnectionIceEvent
+                    ) => {
+                        if (e.candidate === null) {
+                            console.log("offer set", this.pc!.localDescription);
+                            clearTimeout(timeout);
+                            this.state = "middle";
+                            const sessionDescription =
+                                this.pc!.localDescription;
+                            if (sessionDescription === null)
+                                throw Error("failed to get generate answer!");
+                            res(sessionDescription);
+                        }
+                    };
+                }
+            );
+            return JSON.stringify(answer);
+        } catch (err) {
+            return this.initAnswer(offer);
+        }
+    }
+}
